911ef9c06a89e9311ea9f6ba432231864da465d4
Abhra00 <abhramondal9674@gmail.com>
2025-08-03 01:22:49 +0530

    Weather module for waybar added and some decoration update and some script update ✨

--- bin/blocks.sh
+++ bin/blocks.sh
@@ -18,6 +18,7 @@ t=$'\e[0m'
 v=$'\e[7m'
 
 cat <<EOF
+
     $f1███$d$fbright1▄$t  $f2███$d$fbright2▄$t  $f3███$d$fbright3▄$t  $f4███$d$fbright4▄$t  $f5███$d$fbright5▄$t  $f6███$d$fbright6▄$t  $f7███$d$fbright7▄$t  
     $f1███$d$fbright1█$t  $f2███$d$fbright2█$t  $f3███$d$fbright3█$t  $f4███$d$fbright4█$t  $f5███$d$fbright5█$t  $f6███$d$fbright6█$t  $f7███$d$fbright7█$t  
     $f1███$d$fbright1█$t  $f2███$d$fbright2█$t  $f3███$d$fbright3█$t  $f4███$d$fbright4█$t  $f5███$d$fbright5█$t  $f6███$d$fbright6█$t  $f7███$d$fbright7█$t  
