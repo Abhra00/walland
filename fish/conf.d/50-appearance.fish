ecdef1318896d47f241c4222a0d79d963056c02e
Abhra00 <abhramondal9674@gmail.com>
2025-08-09 17:02:53 +0530

    Ditched starship & using pure fish prompt ✨

--- fish/conf.d/50-appearance.fish
+++ fish/conf.d/50-appearance.fish
@@ -6,12 +6,8 @@
 # Source pywal colors
 source ~/.cache/wal/colors.fish
 
-# Prompt & custom colors
+# Custom colors
 if status is-interactive
-
     # custom colors
     command cat ~/.cache/wal/sequences.txt 2> /dev/null
-    
-    # starship prompt
-    starship init fish | source
 end
