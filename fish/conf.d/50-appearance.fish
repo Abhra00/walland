f1f5498709317646a0cc9832ec02a2ae4d98e2de
Abhra00 <abhramondal9674@gmail.com>
2025-08-10 23:15:08 +0530

    Starship is endless possibilities ✨

--- fish/conf.d/50-appearance.fish
+++ fish/conf.d/50-appearance.fish
@@ -6,8 +6,12 @@
 # Source pywal colors
 source ~/.cache/wal/colors.fish
 
-# Custom colors
+# Prompt & custom colors
 if status is-interactive
+
     # custom colors
     command cat ~/.cache/wal/sequences.txt 2> /dev/null
+    
+    # starship prompt
+    starship init fish | source
 end
